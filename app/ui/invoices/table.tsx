942a95efcacc48c2209389b6ce2a130bdf38c034
Presa <ivopresa@gmail.com>
2023-12-12 02:40:38 +0100

    Server Actions to mutate data

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -107,11 +107,9 @@ export default async function InvoicesTable({
                   <td className="whitespace-nowrap px-3 py-3">
                     <InvoiceStatus status={invoice.status} />
                   </td>
-                  <td className="whitespace-nowrap py-3 pl-6 pr-3">
-                    <div className="flex justify-end gap-3">
-                      <UpdateInvoice id={invoice.id} />
-                      <DeleteInvoice id={invoice.id} />
-                    </div>
+                  <td className="flex justify-end gap-2 whitespace-nowrap px-6 py-4 text-sm">
+                    <UpdateInvoice id={invoice.id} />
+                    <DeleteInvoice id={invoice.id} />
                   </td>
                 </tr>
               ))}
