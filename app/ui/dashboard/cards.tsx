04b5178dceda69390e6b4ee64a7a97870c0da9b6
Presa <ivopresa@gmail.com>
2024-06-22 08:06:16 +0200

    Prettier: ``` - cards.tsx - latest-invoices.tsx - revenue-chart.tsx ```

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -53,8 +53,7 @@ export function Card({
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
       <p
-        className={`${lusitana.className}
-          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
+        className={`${lusitana.className} truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
       >
         {value}
       </p>
